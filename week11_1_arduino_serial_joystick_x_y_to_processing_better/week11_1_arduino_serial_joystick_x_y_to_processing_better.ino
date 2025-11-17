caae6753280c4bb8df630bc07bffe45d6f5cec22
RrRun0216 <144758468+RrRun0216@users.noreply.github.com>
2025-11-17 16:43:26 +0800

    week11

---
+++ week11_1_arduino_serial_joystick_x_y_to_processing_better/week11_1_arduino_serial_joystick_x_y_to_processing_better.ino
@@ -0,0 +1,33 @@
+// week11_1_arduino_serial_joystick_x_y_to_processing_better
+// 修改自 week10_7_arduino_serial_joystick_x_y_to_processing
+void setup() {
+  Serial.begin(9600);
+  pinMode(2, INPUT_PULLUP);  
+  pinMode(8, OUTPUT); 
+}
+int count = 0, totalX = 0, totalY = 0; //要統計平均的值
+int x0 = 512, y0 = 512;
+void loop() {
+  delay(30);  // 要慢一點，不然 Processing 會接不了
+  int x = analogRead(A2);   // 把搖桿的 X 接到 A2
+  int y = analogRead(A3);   // 把搖桿的 Y 接到 A3
+  if(count<20) { //想收20筆資料
+    totalX += x; //加總,要算平均值
+    totalY += y; //加總,要算平均值
+    count++; //又讀到一筆
+    x0 = totalX / count; //算平均
+    y0 = totalY / count; //算平均
+  }
+  //Serial.write(x/4);  // 把 0-1023 變成 0-255
+  //Serial.write(y/4);
+  if( abs(x-x0) < 25 ) x = 128; //數值變化太小,直接放中間值128
+  else x = (x-x0)/4.4 + 128; //有大的數值,就減掉中間值,在除4,在加128
+  if( abs(y-y0) < 25 ) x = 128; //數值變化太小,直接放中間值128
+  else x = (y-y0)/4.4 + 128; //有大的數值,就減掉中間值,在除4,在加128
+  Serial.write(x);  
+  Serial.write(y);
+  //if (x > 900) tone(8, 784, 100); 
+  //if (x > 700) tone(8, 523, 100); 
+  //if (x > 500) tone(8, 659, 100); 
+  //if (y > 900) tone(8, 500, 100); //的亂數數字
+}
